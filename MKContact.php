c5691b6969c316d1a95fedcb6ca5d3b3c86fe8a3
mathieknits <48035223+mathieknits@users.noreply.github.com>
2019-09-06 10:26:39 -0400

    Add files via upload

--- MKContact.php
+++ MKContact.php
@@ -1,27 +1,18 @@
-<?php
-if($_POST["message"]) {
-    mail("mathieknits@gmail.com", "Form to email message", $_POST["message"], "From: website_form");
-}
-?>
+<?php 
+if(isset($_POST['form_button'])){
+    $to = "mathieknits@gmail.com"; // this is your Email address
+    $from = $_POST['email_input']; // this is the sender's Email address
+    $first_name = $_POST['name_input'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name_input . " wrote the following:" . "\n\n" . $_POST['message_input'];
+    $message2 = "Here is a copy of your message " . $name_input . "\n\n" . $_POST['message_input'];
 
-<?php
-2
- 
-3
-  $to = "mathieknits@gmail.com";
-4
- 
-5
-  $headers = "From: $email_input \r\n";
-6
- 
-7
-  $headers .= "Reply-To: $email_input \r\n";
-8
- 
-9
-  mail($to,$subject_input,$message_input,$headers);
-10
- 
-11
- ?>
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name_input . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
